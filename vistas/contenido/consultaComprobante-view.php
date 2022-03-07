cf7ef1ec41227e8ee950e99bbe742ddbbfcdcab8
perlaxd365 <prlaxd365@gmail.com>
2022-03-07 11:51:33 -0500

    comprobante titulo xd

--- vistas/contenido/consultaComprobante-view.php
+++ vistas/contenido/consultaComprobante-view.php
@@ -1,7 +1,8 @@
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <div class="pcoded-content">
     <style>
         /* Styles for wrapping the search box */
@@ -105,7 +106,7 @@
                                         </div>
                                         <div class="col-6">
 
-                                            <label for="exampleInputEmail1">Tipo de Comprobante</label>
+                                            <label for="exampleInputEmail1">Tipo Comprobante</label>
                                             <select name="consulta_tipo_comp" class="custom-select">
                                                 <option selected value="01">Factura electronica</option>
                                                 <option value="03">Boleta de venta electronia</option>
